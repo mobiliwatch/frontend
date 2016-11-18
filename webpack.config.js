9e6847c1ad29ae5c80a40e39589bf23b4aa7e09b
Bastien Abadie <bastien@nextcairn.com>
2016-11-18 15:01:29 +0000

    Restore webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require("webpack");
 
 var DEBUG = process.env.NODE_ENV == 'dev';
-//var API_URL = DEBUG ? 'http://localhost:8000/api' : 'https://mobili.watch/api';
-var API_URL = 'https://mobili.watch/api';
+var API_URL = DEBUG ? 'http://localhost:8000/api' : 'https://mobili.watch/api';
 
 module.exports = {
   entry: 'index.js',
